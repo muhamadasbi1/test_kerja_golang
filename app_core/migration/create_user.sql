0b57c5ef15500d1d7fd83d481d707b833c0f442f
muhamad.asbi1 <muhamad.asbi@global-infotech.co.id>
2024-10-25 02:58:23 +0700

    Emptying the main branch

--- app_core/migration/create_user.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO auth_users (name, password, email)
-VALUES ('admin', 'admin', 'admin@localhost') ON DUPLICATE KEY
-UPDATE email = email;
--- item_module/dto/category.dto.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type CategoryCreateRequest struct {
-	Name string `json:"name" validate:"required"`
-}
-
-type CategoryUpdateRequest struct {
-	Name string `json:"name" validate:"required"`
-}
